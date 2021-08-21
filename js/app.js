427f0270fd9f056ad201c133f95eb04fd998fc7c
keylighter <keysuggest@gmail.com>
2021-08-21 20:09:54 +0600

    mac-book

---
+++ js/app.js
@@ -0,0 +1,146 @@
+
+
+//storage ,memory,delivery feature cost calculate 
+
+function updateFeature(feature, price, isBig) {
+    const featureCost = document.getElementById(feature + '-cost');
+    if (isBig == true) {
+        featureCost.innerText = price;
+    }
+    else if (isBig == false) {
+        featureCost.innerText = price;
+    }
+    else {
+        featureCost.innerText = price;
+    }
+    calculateTotal();
+}
+
+//applying the promocode and get final price
+
+const promoButton = document.getElementById('promo-button');
+promoButton.addEventListener('click', function () {
+    let totalPrice = document.getElementById('total-price');
+    const promoInput = document.getElementById('promo-input');
+    const promoInputValue = promoInput.value;
+    const subTotal = document.getElementById('sub-total');
+    if (promoInputValue == 'stevekaku') {
+        totalPrice = parseInt((subTotal.innerText * 80) / 100);
+
+        console.log(totalPrice);
+
+        document.getElementById('total-price').innerText = totalPrice;
+        promoInput.value = '';
+    }
+
+})
+
+//calculate subtotal cost
+
+function calculateTotal() {
+    const bestPrice = document.getElementById('best-price').innerText;
+    const memoryCost = document.getElementById('memory-cost').innerText;
+    const storageCost = document.getElementById('storage-cost').innerText;
+    const deliveryCost = document.getElementById('delivery-cost').innerText;
+
+    const subTotal = parseInt(bestPrice) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost);
+
+    document.getElementById('sub-total').innerText = subTotal;
+
+}
+
+// delivery  calculate 
+
+document.getElementById('fast-delivery').addEventListener('click', function () {
+
+    updateFeature('delivery', 20, true);
+})
+document.getElementById('late-delivery').addEventListener('click', function () {
+
+    updateFeature('delivery', 0, false);
+
+})
+
+// storage calculation 
+
+document.getElementById('small-storage').addEventListener('click', function () {
+    updateFeature('storage', 0)
+})
+document.getElementById('medium-storage').addEventListener('click', function () {
+    updateFeature('storage', 100, true);
+})
+document.getElementById('large-storage').addEventListener('click', function () {
+    updateFeature('storage', 180, false);
+})
+
+//memory  calculate
+
+document.getElementById('small-memory').addEventListener('click', function () {
+    updateFeature('memory', 0, true);
+})
+
+document.getElementById('medium-memory').addEventListener('click', function () {
+    updateFeature('memory', 180, true);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* // delivery cost calculate
+
+document.getElementById('fast-delivery').addEventListener('click', function () {
+    const deliveryCost = document.getElementById('delivery-cost');
+
+    deliveryCost.innerText = 20;
+})
+document.getElementById('late-delivery').addEventListener('click', function () {
+    const deliveryCost = document.getElementById('delivery-cost');
+
+    deliveryCost.innerText = 0;
+})
+
+//storage cost calculate
+
+document.getElementById('small-storage').addEventListener('click', function () {
+    const storageCost = document.getElementById('storage-cost');
+    storageCost.innerText = 0;
+})
+document.getElementById('medium-storage').addEventListener('click', function () {
+    const storageCost = document.getElementById('storage-cost');
+    storageCost.innerText = 100;
+})
+document.getElementById('large-storage').addEventListener('click', function () {
+    const storageCost = document.getElementById('storage-cost');
+    storageCost.innerText = 180;
+})
+
+//memory cost calculate
+
+document.getElementById('small-memory').addEventListener('click', function () {
+    const memoryCost = document.getElementById('memory-cost');
+    memoryCost.innerText = 0;
+})
+
+document.getElementById('medium-memory').addEventListener('click', function () {
+    const memoryCost = document.getElementById('memory-cost');
+    memoryCost.innerText = 180;
+}) */
